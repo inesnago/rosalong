bc1428814eaab5bb6bbb95b19b667cbdbd38e2cd
inesnago <imsnago@gmail.com>
2020-10-22 20:55:02 +0000

    initial commit

---
+++ js/myscripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $("#boards").click(function(){
+        $("#boards-main").fadeToggle("fast")
+    })
+
+    $("#my-modal-btn-1").click(function(){
+        $("#my-commerce").show("fast")
+    })
+
+});
