f63ecb292f532334c1cde9640f95f08b8d25105b
inesnago <imsnago@gmail.com>
2020-10-22 22:40:18 +0000

    working on modal

--- js/myscripts.js
+++ js/myscripts.js
@@ -2,9 +2,24 @@ $(document).ready(function(){
     $("#boards").click(function(){
         $("#boards-main").fadeToggle("fast")
     })
-
-    $("#my-modal-btn-1").click(function(){
-        $("#my-commerce").show("fast")
-    })
-
 });
+
+
+
+var btn = document.getElementById("my-modal-btn-1")
+var modal = document.getElementById("my-commerce")
+var close = document.getElementById("close-button-1")
+
+btn.onclick = function(){
+    modal.style.display = "block"
+};
+
+close.onclick = function(){
+    modal.style.display = "none"
+};
+
+window.onclick = function(event){
+    if (event.target == modal){
+        modal.style.display = "none"
+    }
+};
